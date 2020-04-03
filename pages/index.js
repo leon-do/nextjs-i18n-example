f535ec11c9f8182712cf80555214f35852b9a0f5
leon-do <leondo123@gmail.com>
2020-04-03 13:48:04 -0400

    i18n

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
-const Home = () => (
-  <div> 
-      helo
-  </div>
-)
+import { withTranslation, i18n } from "../i18n";
+import PropTypes from "prop-types";
 
-export default Home
+const Home = ({ t }) => {
+  return (
+    <div>
+      <button onClick={() => i18n.changeLanguage("en")}> en </button>
+      <button onClick={() => i18n.changeLanguage("de")}> de </button>
+      {t("hello")}
+    </div>
+  );
+};
+
+Home.getInitialProps = async () => ({
+  namespacesRequired: ['common'],
+})
+
+Home.propTypes = {
+  t: PropTypes.func.isRequired,
+};
+
+export default withTranslation("common")(Home);
