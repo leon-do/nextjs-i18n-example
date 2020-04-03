945560cbb07d7395c77b98b2c1d48b918282ce18
leon-do <leondo123@gmail.com>
2020-04-03 14:06:49 -0400

    rm more shit

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,8 @@
 // https://github.com/isaachinman/next-i18next#3-project-setup
 // https://github.com/isaachinman/next-i18next/blob/master/examples/simple/pages/_app.js
-import React from 'react'
-import App from 'next/app'
-import { appWithTranslation } from '../i18n'
+import App from "next/app";
+import { appWithTranslation } from "../i18n";
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props
-    return (
-      <Component {...pageProps} />
-    )
-  }
-}
+class MyApp extends App {}
 
-export default appWithTranslation(MyApp)
+export default appWithTranslation(MyApp);
